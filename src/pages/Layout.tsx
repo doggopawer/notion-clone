aa12de6b84802e0de1479f21086a11887b1b3147
doggopawer <doggopawer@gmail.com>
2024-02-28 23:39:32 +0900

    feat: 기본 레이아웃 세팅

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,6 +1,61 @@
 import { useContext } from "react";
 import { Outlet } from "react-router-dom";
 import { ThemeContext, ThemeContextType } from "../context/themeContext";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100%;
+`;
+const Container = styled.div`
+  max-width: 640px;
+  margin: 0 auto;
+  padding: 0 16px;
+`;
+const Header = styled.div`
+  width: 100%;
+  height: 77px;
+  background: tomato;
+  position: fixed;
+  top: 0;
+  z-index: 999;
+`;
+const HeaderContainer = styled(Container)`
+  display: flex;
+  height: 100%;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.div`
+  color: ${(props) => props.theme.textColor};
+  font-size: 24px;
+  font-weight: 600;
+`;
+const Content = styled.div`
+  padding-top: 77px;
+  width: 100%;
+  height: 100vh;
+`;
+const ContentContainer = styled(Container)`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  &::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera에서 스크롤바 숨기기 */
+  }
+`;
+
+const FabContainer = styled.div`
+  position: absolute;
+  right: 16px;
+  bottom: calc(56px + 30px);
+  width: 56px;
+`;
+
+const FabButton = styled.button`
+  position: fixed;
+`;
 
 const Layout = () => {
   const { themeMode, setThemeMode } = useContext(
@@ -16,11 +71,22 @@ const Layout = () => {
   };
 
   return (
-    <div>
-      <div>레이아웃</div>
-      <button onClick={handleToggleThemeMode}>다크/라이트모드</button>
-      <Outlet />
-    </div>
+    <Wrapper>
+      <Header>
+        <HeaderContainer>
+          <Logo>Notable.</Logo>
+          <button onClick={handleToggleThemeMode}>다크/라이트모드</button>
+        </HeaderContainer>
+      </Header>
+      <Content>
+        <ContentContainer>
+          <Outlet />
+          <FabContainer>
+            <FabButton>+</FabButton>
+          </FabContainer>
+        </ContentContainer>
+      </Content>
+    </Wrapper>
   );
 };
 export default Layout;
--- src/theme/GlobalStyle.ts
+++ src/theme/GlobalStyle.ts
@@ -3,8 +3,12 @@ import reset from "styled-reset";
 
 export const GlobalStyle = createGlobalStyle`
 	${reset}
-	body{
-		background:${(props) => props.theme.backgroundColor};
-        color: ${(props) => props.theme.textColor};
+	* {
+		box-sizing: border-box;
 	}
+	body{
+		background: ${(props) => props.theme.backgroundColor};
+        color: ${(props) => props.theme.textColorColor};
+	}
+
 `;
