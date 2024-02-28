f0d5ac66a1381b377e81fbe7ac8bb877d407a24f
doggopawer <doggopawer@gmail.com>
2024-02-29 03:00:54 +0900

    feat: Document 타입 정의

---
+++ src/types/document.ts
@@ -0,0 +1,29 @@
+export type Document = {
+  id: number;
+  title: string;
+  createdAt: Date;
+  updatedAt: Date;
+  documents: Document[];
+};
+
+export type GetDocumentsResponse = Document[];
+export type GetDoucmentByIdResponse = Document & { content: string };
+export type PostDocumentRequest = {
+  title: string;
+  parent: number | null;
+};
+export type PostDocumentResponse = Omit<Document, "documents">;
+export type PutDocumentRequest = {
+  title: string;
+  content: string;
+};
+export type PutDocumentResponse = {
+  id: number;
+  title: string;
+  content: string;
+  parent: number | null;
+  username: string;
+  created_at: Date;
+  updated_at: Date;
+};
+export type DeleteDocumentResponse = PutDocumentResponse;
