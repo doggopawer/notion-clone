5508e14b7a840058685c37c4183529f7039c0010
doggopawer <doggopawer@gmail.com>
2024-03-07 00:01:25 +0900

    feat: DocumentItem 삭제 선택 기능 - handlePushDeleteId 적용 - handleNavigateToDocument 제작 및 적용 - isDeleteMode 별 작동 핸들러 분기 - active일 때 CSS 적용

--- src/components/business/DocumentItem.tsx
+++ src/components/business/DocumentItem.tsx
@@ -1,25 +1,35 @@
 import styled from "styled-components";
 import { faFile } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "react-router-dom";
+
 import { Document } from "types/document";
+import { useContext } from "react";
+import {
+  DeleteModeContext,
+  DeleteModeContextType,
+} from "context/DeleteModeContext";
+import { useNavigate } from "react-router-dom";
 
 const Wrapper = styled.li`
   position: relative;
   border: 1.5px solid ${(props) => props.theme.secondary};
   background: ${(props) => props.theme.primary};
-  margin-bottom: 24px;
-`;
-
-const Anchor = styled(Link)`
   color: ${(props) => props.theme.secondary};
-  text-decoration: none;
-  width: 100%;
+  margin-bottom: 24px;
   height: 70px;
   display: flex;
   align-items: center;
   position: relative;
   overflow: hidden;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  transform: rotateX(0);
+  &.active {
+    transform: rotateX(360deg);
+    border: 1.5px solid ${(props) => props.theme.primary};
+    background: ${(props) => props.theme.secondary};
+    color: ${(props) => props.theme.primary};
+  }
 `;
 
 const Title = styled.h3`
@@ -42,13 +52,26 @@ type DocumentItemProps = {
 
 const DocumentItem = ({ document }: DocumentItemProps) => {
   const { id } = document;
+  const navigate = useNavigate();
+  const { deleteIdList, handlePushDeleteId, isDeleteMode } = useContext(
+    DeleteModeContext
+  ) as DeleteModeContextType;
+
+  const isDeleted = deleteIdList.includes(id);
+
+  const handleNavigateToDocument = () => {
+    navigate(`/documents/${id}`);
+  };
 
   return (
-    <Wrapper>
-      <Anchor to={`/documents/${id}`}>
-        <DocumentIcon />
-        <Title>{document.title}</Title>
-      </Anchor>
+    <Wrapper
+      className={isDeleted ? "active" : ""}
+      onClick={
+        isDeleteMode ? () => handlePushDeleteId(id) : handleNavigateToDocument
+      }
+    >
+      <DocumentIcon />
+      <Title>{document.title}</Title>
     </Wrapper>
   );
 };
