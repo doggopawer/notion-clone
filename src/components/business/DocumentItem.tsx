3fc00befbf92d0fe7dfa9a820828825b01b47059
doggopawer <doggopawer@gmail.com>
2024-03-06 21:20:32 +0900

    remove: DeleteDocumentContent 삭제

--- src/components/business/DocumentItem.tsx
+++ src/components/business/DocumentItem.tsx
@@ -1,11 +1,8 @@
-import styled, { css } from "styled-components";
-import { faFile, faTrash } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import { Document } from "types/document";
-import Modal from "components/ui/Modal/Modal";
-import Portal from "components/ui/Portal";
-import DeleteDocumentContent from "./DeleteDocumentContent";
 
 const Wrapper = styled.li`
   position: relative;
@@ -30,26 +27,7 @@ const Title = styled.h3`
   font-weight: 600;
   margin-left: 40px;
 `;
-const SlideToDeleteButton = styled.div`
-  width: 80px;
-  position: absolute;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  color: ${(props) => props.theme.primary};
-  background: ${(props) => props.theme.secondary};
-  border-radius: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  transition: transform 0.5s ease;
-  transform: translateX(100%);
 
-  &:hover {
-    transform: translateX(0);
-  }
-`;
 const DocumentIcon = () => {
   const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
     margin-left: 32px;
@@ -57,19 +35,6 @@ const DocumentIcon = () => {
   `;
   return <StyledFontAwesomeIcon icon={faFile} />;
 };
-const DeleteIcon = () => {
-  const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
-    font-size: 24px;
-  `;
-  return <StyledFontAwesomeIcon icon={faTrash} />;
-};
-
-const triggerButtonCSS = css`
-  border: none;
-  cursor: pointer;
-  background: ${({ theme }) => theme.secondary};
-  color: ${({ theme }) => theme.primary};
-`;
 
 type DocumentItemProps = {
   document: Document;
@@ -84,21 +49,6 @@ const DocumentItem = ({ document }: DocumentItemProps) => {
         <DocumentIcon />
         <Title>{document.title}</Title>
       </Anchor>
-      {
-        <SlideToDeleteButton>
-          <Modal>
-            <Modal.Trigger css={triggerButtonCSS}>
-              <DeleteIcon />
-            </Modal.Trigger>
-            <Portal>
-              <Modal.Content>
-                <DeleteDocumentContent documentId={document.id} />
-              </Modal.Content>
-              <Modal.Backdrop />
-            </Portal>
-          </Modal>
-        </SlideToDeleteButton>
-      }
     </Wrapper>
   );
 };
