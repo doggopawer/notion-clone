cce20e16fb98d4f27dd255a49436dad6a6189f1d
doggopawer <doggopawer@gmail.com>
2024-03-06 22:48:46 +0900

    feat: DeleteModeProvider 전역 적용

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import ThemeProvider from "./context/ThemeContext";
 import { GlobalStyle } from "./theme/GlobalStyle";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import DeleteModeProvider from "context/DeleteModeContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -15,12 +16,14 @@ const queryClient = new QueryClient();
 
 root.render(
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider>
-      <GlobalStyle />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <DeleteModeProvider>
+      <ThemeProvider>
+        <GlobalStyle />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </DeleteModeProvider>
     <ReactQueryDevtools />
   </QueryClientProvider>
 );
